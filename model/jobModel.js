050b25cb90a543328c5b1c5cccc58cb299027bd7
Adith <adith@suvarnas.com>
2024-01-26 22:39:06 +0530

    Completed the backend parts

--- model/jobModel.js
+++ model/jobModel.js
@@ -1,53 +1,54 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const jobSchema = new mongoose.Schema({
     companyName: {
-        type : String,
-        require: true,
+        type: String,
+        required: true,
     },
     logoUrl: {
-        type : String,
-        require: true,
+        type: String,
+        required: true,
     },
     position: {
-        type : String,
-        require: true,
+        type: String,
+        required: true,
     },
     salary: {
-        type : String,
-        require: true,
+        type: String,
+        required: true,
     },
     jobType: {
-        type : String,
-        require: true,
+        type: String,
+        required: true,
     },
     remote: {
-        type : String,
-        require: true,
+        type: String,
+        required: true,
     },
     location: {
-        type : String,
-        require: true,
+        type: String,
+        required: true,
     },
     desc: {
-        type : String,
-        require: true,
+        type: String,
+        required: true,
     },
     about: {
         type: String,
-        require: true,
+        required: true,
     },
     skills: {
-        type : String,
-        require: true,
+        type: [String], // Assuming skills is an array of strings
+        required: true,
     },
     info: {
-        type : String,
-        require: true,
+        type: String,
+        required: true,
     },
     userRefId: {
-        type : mongoose.Types.ObjectId,
-        require: true,
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
     },
-})
+});
 
 module.exports = mongoose.model("jobs", jobSchema);
--- routes/job.js
+++ routes/job.js
@@ -4,7 +4,7 @@ const jobValidation = require('../validations/jobValidation');
 const Job = require('../model/jobModel');
 const verifyJwt = require('../middleware/jwtMiddleware')
 
-router.post('/jobs', verifyJwt,async (req,res) => {
+router.post('/jobs', verifyJwt, async (req,res) => {
     try {
         // Validate the request body using Joe
         const validationResult = jobValidation.validate(req.body);
@@ -23,5 +23,84 @@ router.post('/jobs', verifyJwt,async (req,res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
+
+
+router.put('/edit/:jobId', verifyJwt, async (req,res) => {
+    try {
+        // Validate the request body using Joe
+        const validationResult = jobValidation.validate(req.body);
+
+        // Check if validation passed
+        if (validationResult.error) {
+            return res.status(400).json({ error: validationResult.error.message });
+        }
+        let jobId = req.params.jobId 
+        let newData = req.body;
+        await Job.updateOne({ _id : jobId},{
+            $set : newData,
+        })
+
+        res.status(201).json({message : "Job Updated Successfully"});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.get('/findOne/:jobId', async (req, res) => {
+    try {
+        let jobId = req.params.jobId;
+        
+        if (!jobId) {
+            return res.status(400).json({ errorMessage: "Required Job Id " });
+        }
+
+        //send data without id , 1 if needed & 0 if not needed
+        const jobData = await Job.findById(jobId);
+
+        if (!jobData) {
+            return res.status(404).json({ message: "Job not found" });
+        }
+
+        res.status(200).json({ data: jobData });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+//get all jobs
+router.get('/all', async (req, res) => {
+    try {
+        const position = req.query.position || "";
+        let skills = req.query.skills || null; // Use null if skills are not provided
+
+        // Check if skills is a string, and if so, split it into an array
+        if (typeof skills === 'string') {
+            skills = skills.split(',').map(skill => skill.trim());
+        }
+
+        // Define the filter based on whether skills are provided or not
+        const filter = skills ? { position: { $regex: position, $options: "i" }, skills: { $in: skills } }
+                              : { position: { $regex: position, $options: "i" } };
+
+        // Send data without id, 1 if needed & 0 if not needed
+        const jobData = await Job.find(filter, { _id: 1, companyName: 1 });
+
+        if (!jobData || jobData.length === 0) {
+            return res.status(404).json({ message: "Job not found" });
+        }
+
+        res.status(200).json({ data: jobData });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+
+
+
 
 module.exports = router;
